1dcb67d1bbae130c254b07109aaaf3e1fd0c744c
NICOLLAS JOSE PRIM <nicollas_prim@estudante.sesisenai.org.br>
2025-07-11 14:55:23 -0300

    ajuste conn

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,18 +1,16 @@
 const { Sequelize } = require('sequelize')
-require('dotenv').config()
+require('dotenv').config({ path: __dirname + '/.env', override: true })
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+console.log('DEBUG DB_USER:', process.env.DB_USER) 
+
+const sequelize = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'mysql'
+    dialect: 'mysql',
 })
 
-sequelize.authenticate() 
-.then(()=> {
-    console.log('Conexão com o banco rodando!')
-})
-.catch((err)=>{
-    console.error('Conexão com o banco falhou!')
-})
+sequelize.authenticate()
+  .then(() => console.log('Conexão com o banco rodando!'))
+  .catch((err) => console.error('Conexão com o banco falhou!', err))
 
 module.exports = sequelize
